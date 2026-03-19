853d1ffc4f52aed114620dc5a49fb3f97843f2c8
hiziaa <rawat.hizia@gmail.com>
2026-03-19 14:30:12 +0100

    ajout debug

--- Assets/PlayerPositionDebug.cs
+++ Assets/PlayerPositionDebug.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
-
+ 
+ 
 public class PlayerPositionDebug : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    #if UNITY_EDITOR
+    [SerializeField]
+    private Transform debugTransform;
+ 
+ 
+    void Awake()
     {
-        
+        if (debugTransform)
+        {
+            transform.position = debugTransform.position;
+        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    #endif
 }
