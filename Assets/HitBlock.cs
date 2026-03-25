9b47871c3dd683c30da7b54c4e06200fab4c37c7
hiziaa <rawat.hizia@gmail.com>
2026-03-25 14:03:45 +0100

    Ajout boite transparente

--- Assets/HitBlock.cs
+++ Assets/HitBlock.cs
@@ -3,9 +3,31 @@ using UnityEngine;
 public class HitBlock : MonoBehaviour
 {
     private Animator animator;
+    private SpriteRenderer sr;
+ 
+    private PlatformEffector2D pe2d;
+ 
+    [SerializeField]
+    private GameObject itemPrefabStored;
+ 
+    [SerializeField]
+    private bool isHidden = false;
+ 
     void Awake()
     {
         animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+        pe2d = GetComponent<PlatformEffector2D>();
+ 
+        if (isHidden)
+        {
+            sr.enabled = false;
+            pe2d.enabled = true;
+        } else
+        {
+            sr.enabled = true;
+            pe2d.enabled = false;
+        }
     }
  
     void OnCollisionEnter2D(Collision2D collision)
@@ -19,9 +41,24 @@ public class HitBlock : MonoBehaviour
  
         if (contactPoint.normal.y > 0.5f)
         {
+            sr.enabled = true;
+            pe2d.enabled = false;
+ 
             animator.SetTrigger("Hit");
+ 
+            if (itemPrefabStored != null)
+            {
+                GameObject itemStored = Instantiate(
+                    itemPrefabStored,
+                    transform.position,
+                    Quaternion.identity
+                );
+                Collectible itemCollectible = itemStored.GetComponent<Collectible>();
+                //Activation de l'animation
+                itemCollectible.Picked();
+
+            }
         }
     }
 }
- 
  
