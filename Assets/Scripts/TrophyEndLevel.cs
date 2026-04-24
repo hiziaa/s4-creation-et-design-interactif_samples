415cf23ca3d724fa75471f6dfd1e7cd12aeb7429
hiziaa <rawat.hizia@gmail.com>
2026-04-24 23:33:29 +0200

    Script pour ajout du trophée final

--- Assets/Scripts/TrophyEndLevel.cs
+++ Assets/Scripts/TrophyEndLevel.cs
@@ -2,15 +2,11 @@ using UnityEngine;
 
 public class TrophyEndLevel : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.CompareTag("Player"))
+        {
+            FindObjectOfType<CurrentSceneManager>().LoadScene("Level2");
+        }
     }
 }
