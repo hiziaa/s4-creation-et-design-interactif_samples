3f4485218eb8a47cf69988a17e122f78845c9141
hiziaa <rawat.hizia@gmail.com>
2026-05-06 22:22:01 +0200

    Checkpoint devient gris une fois qu'on le passe + Ajout des modifs sur checkpoint (1) & (2)

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     public BoxCollider2D bc2d;
+    private SpriteRenderer sr;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -12,6 +19,8 @@ public class Checkpoint : MonoBehaviour
             {
                 playerSpawn.currentSpawnPosition = transform.position;
                 bc2d.enabled = false;
+                // Griser le sprite
+                sr.color = new Color(0.5f, 0.5f, 0.5f, 1f);
             }
         }
     }
